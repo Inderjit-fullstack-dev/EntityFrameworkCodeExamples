8319b54239240b3ea46618b81121a901f561ec13
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2023-01-23 20:04:50 +0530

    entity framework core is added with category table migration

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using EntityFramworkCoreCompleteDemo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFramworkCoreCompleteDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
